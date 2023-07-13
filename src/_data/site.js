b284c2537c1a11e5cc29e06ddd3b0ee86a69a082
BrahianVT <pumasemj@hotmail.com>
2023-07-12 21:14:53 -0500

    added url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'AI-deas',
-  description: 'What can do with AI?',
+  title: 'Tips AI',
+  description: 'What can do or automate with AI?',
   keywords: ['ai', 'Ai', 'Chat-gpt', 'artificial', "intelligence","Midjourney"],
-  url: 'https://vredeburg.netlify.app', // your site url without trailing slash
+  url: 'tips-ai.com', // your site url without trailing slash
   paginate: 12,
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
