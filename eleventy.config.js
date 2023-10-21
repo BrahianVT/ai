aeb2f7716adc3ee667e784936f4ba3e3bb3c9c3f
BrahianVT <pumasemj@hotmail.com>
2023-10-20 22:18:10 -0600

    added recomendations

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,6 +10,37 @@ module.exports = (config) => {
   config.addFilter('readableDate', require('./lib/filters/readableDate'));
   config.addFilter('minifyJs', require('./lib/filters/minifyJs'));
 
+  config.addFilter("getRandom5", function(items){
+    if(!items.length || items.length < 5) return;
+
+    var selected = []
+	for(var i = 0; i < 5; i++){
+        selected.push(items[Math.floor(Math.random() * items.length)]);
+    }    
+    
+
+	return selected;
+
+  });
+
+  config.addFilter("getRandom10byCategory", function(items, tags){
+    if(!items.length || items.length < 10) return;
+
+    var elementsInTag = items.filter(i => {
+        let isNext = i.data.tags.some(item => tags.includes(item));
+        return isNext;
+    });
+
+    console.log(elementsInTag.length)
+    var selected = []
+	for(var i = 0; i < 10; i++){
+        selected.push(elementsInTag[Math.floor(Math.random() * elementsInTag.length)]);
+    }    
+    
+
+	return selected;
+
+  });
   config.addTransform('minifyHtml', require('./lib/transforms/minifyHtml'));
 
   config.addCollection('posts', require('./lib/collections/posts'));
